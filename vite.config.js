e20f9f84f0532460deaa9c76df57de928cf086dc
Mario3813 <mario.delrosario@minerd.gob.do>
2026-04-16 22:55:16 -0400

    google search console verification

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <-- Muy importante: asegura que las rutas sean relativas a la raíz
 })
